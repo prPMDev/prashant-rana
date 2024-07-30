cea76b66f5017aa7dcae162fd57f34303055cc23
prPMDev <129408157+prPMDev@users.noreply.github.com>
2024-07-30 11:43:29 -0700

    adding tracking

--- analytics.js
+++ analytics.js
@@ -1,10 +1,2 @@
-window.heap=window.heap||[],heap.load=function(e,t){
-  window.heap.appid=e,heap.config=t=t||{};
-  var r=document.createElement("script");
-  r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";
-  var a=document.getElementsByTagName("script")[0];
-  a.parentNode.insertBefore(r,a);
-  for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},
-  p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)
-  n(p[o])};
-  heap.load("3713320489");
+ window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
+ heap.load("3713320489");
